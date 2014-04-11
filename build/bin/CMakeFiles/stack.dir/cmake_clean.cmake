839be0f1bcf709c566a85f737490f453185d4565
WuNL <wunailiang@gmail.com>
2014-04-11 16:34:25 +0800

    build

---
+++ build/bin/CMakeFiles/stack.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/stack.dir/main.cpp.o"
+  "CMakeFiles/stack.dir/stack.cpp.o"
+  "stack.pdb"
+  "stack"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/stack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
